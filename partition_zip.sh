e084decab590bcb8d012472e7257ba2d120096f5
Erick Platero <40013722+Erick7451@users.noreply.github.com>
2019-08-06 19:13:02 -0500

    Update partition_zip.sh

--- partition_zip.sh
+++ partition_zip.sh
@@ -5,7 +5,7 @@ current_memory=0 #based on MB
 partition=$((1024*5))
 
 #Function
-K_MB() #define variables WITHIN the function. R, all variables are GLOBAL here
+K_MB() 
 {
 if [[ $1 == *K* ]];then #if the variable contains K for KB, then
 	kb=$1 #replace argument so that we can use default awk $1
@@ -19,27 +19,26 @@ fi #end conditional
 
 echo $mb #return statement
 
-} #End of function! WORKS!
+} 
 
 
 #be on dir containing all subject id tags
-ls -lh ./* | grep total | cut -d' ' -f2 | while read f; #gets the
+ls -lh ./* | grep total | cut -d' ' -f2 | while read f; 
 
-#loops goes per DIRECTORY
-# do NOT need indentations, only a NEW line.
+#loops goes per directory
 do
-	f=$(K_MB $f) #notice how I did this
-	((iters += 1)) #counts first iterations
-	current_memory=$(echo $current_memory + $f | bc) #bc combines them arithmetically
+	f=$(K_MB $f) 
+	((iters += 1)) 
+	current_memory=$(echo $current_memory + $f | bc)
 	if [[ $current_memory > $partition ]]; then #double parenthesis does NOT work with comparing FLOAT numbers. needs to be > WITHOUT the '=' b/c of floater
 		((dir += 1)) #add first partition of folders
 		if ((dir == 1)); then
 			ls | head -$iters > folders #directory folders
-			cat folders | xargs -I % zip -r "partition$dir" % #compresses all files in 'cat folders'. this MIGHT work
+			cat folders | xargs -I % zip -r "partition$dir" % 
 			
 		else
 			ls | grep -vf folders | head -$iters >> folders #appends list
-			ls | grep -vf folders | head -$iters | xargs -I % zip -r "partition$dir" % #coprress all files NOT in 'cat folders' until head CONSTRAINT (limit)
+			ls | grep -vf folders | head -$iters | xargs -I % zip -r "partition$dir" % #coprress all files NOT in 'cat folders' until head CONSTRAINT (lim
 			
 		fi #end condition and give control to INNER loop
 
